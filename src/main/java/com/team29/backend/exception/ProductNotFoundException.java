6121b7ccf29463dee1ec92d3da79f26fe2374e48
KhaledQasim <khaled.qasim183@gmail.com>
2023-02-11 15:36:23 +0000

    Update ProductNotFoundException.java

--- src/main/java/com/team29/backend/exception/ProductNotFoundException.java
+++ src/main/java/com/team29/backend/exception/ProductNotFoundException.java
@@ -2,7 +2,9 @@ package com.team29.backend.exception;
 
 public class ProductNotFoundException extends RuntimeException {
 
-    public ProductNotFoundException(Long id) {
+    private static final long serialVersionUID = 1706878377314510641L;
+
+	public ProductNotFoundException(Long id) {
         super("Could not find the product matching the given Id: " + id);
     }
     
