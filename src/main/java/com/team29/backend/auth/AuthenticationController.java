33b997f4e4b915e55270d0aab6cfa601fd8fc4f3
KhaledQasim <khaled.qasim183@gmail.com>
2023-02-19 13:18:57 +0000

    Changed crossOrigin back to default values
    Cross Origins now only allows local host, need to add the https url when the server is ready for build/deploy

--- src/main/java/com/team29/backend/auth/AuthenticationController.java
+++ src/main/java/com/team29/backend/auth/AuthenticationController.java
@@ -1,6 +1,7 @@
 package com.team29.backend.auth;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = {"http://localhost:3000/", "http://localhost:8080"}, allowCredentials = "true")
 public class AuthenticationController {
 
     private final AuthenticationService service;
