7da109692cf84fa5a1e5c850445c8014fc8973c6
KhaledQasim <khaled.qasim183@gmail.com>
2023-03-27 08:39:21 +0100

    commented out bad code

--- src/main/java/com/team29/backend/dto/checkout/StripeResponse.java
+++ src/main/java/com/team29/backend/dto/checkout/StripeResponse.java
@@ -1,19 +1,19 @@
-package com.team29.backend.dto.checkout;
+// package com.team29.backend.dto.checkout;
 
-public class StripeResponse {
-    private String sessionId;
+// public class StripeResponse {
+//     private String sessionId;
 
 
 
-    public StripeResponse(String sessionId){
-        this.sessionId = sessionId;
-    }
+//     public StripeResponse(String sessionId){
+//         this.sessionId = sessionId;
+//     }
 
-    public String getSessionId() {
-        return sessionId;
-    }
+//     public String getSessionId() {
+//         return sessionId;
+//     }
 
-    public void setSessionId(String sessionId) {
-        this.sessionId = sessionId;
-    }
-}
+//     public void setSessionId(String sessionId) {
+//         this.sessionId = sessionId;
+//     }
+// }
